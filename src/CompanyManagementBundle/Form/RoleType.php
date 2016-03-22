731ba66c6eb7832d27e2809a52ab584836c6545d
badamziom <badamziom@o2.pl>
2016-03-22 13:45:03 +0100

    Make crud for user and role

---
+++ src/CompanyManagementBundle/Form/RoleType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CompanyManagementBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RoleType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('title')
+            ->add('description')
+            ->add('acl')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CompanyManagementBundle\Entity\Role'
+        ));
+    }
+}
