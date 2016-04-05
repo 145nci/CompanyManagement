fbc4ef7c7d1e611de2d87058068922dd2b257d7a
badamziom <badamziom@o2.pl>
2016-04-05 13:53:01 +0200

    Changes

---
+++ src/CompanyManagementBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 05.04.16
+ * Time: 13:28
+ */
+namespace CompanyManagement\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use CompanyManagementBundle\Entity\User;
+
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPassword('test');
+
+        $userAdmin->setCompany($this->getReference('company_1'));
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
