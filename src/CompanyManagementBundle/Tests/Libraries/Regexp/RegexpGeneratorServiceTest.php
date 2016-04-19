69668dc90edc628b6abb5dd66430a5702894bcdb
Wojciech Pruszak <setzo94@gmail.com>
2016-04-19 11:20:55 +0200

    s

--- src/CompanyManagementBundle/Tests/Libraries/Regexp/RegexpGeneratorServiceTest.php
+++ src/CompanyManagementBundle/Tests/Libraries/Regexp/RegexpGeneratorServiceTest.php
@@ -23,8 +23,6 @@ class RegexpGeneratorServiceTest extends \Symfony\Bundle\FrameworkBundle\Test\We
 
         $expression = $regexpService->matchingAll(array('wyraz1, wyraz2'));
 
-        var_dump($expression);
-
         $this->assertGreaterThan(0, preg_match(
                 $expression,
                 'wyraz1
